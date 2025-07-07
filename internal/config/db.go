5ec09c0dbef0b484a8710e021b642c9ea9f56adf
Shaggy Mission <rescuefriendsuio@gmail.com>
2025-07-06 20:55:47 -0500

    Update db.go

--- internal/config/db.go
+++ internal/config/db.go
@@ -10,12 +10,14 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() *gorm.DB {
-    dsn := "admin:Lis.12345@tcp(shaggymission-db-server.cd5kwh1iyrrb.us-east-1.rds.amazonaws.com:3306)/user_service_db?charset=utf8mb4&parseTime=True&loc=Local"
-    database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    dsn := "admin:Lis.12345@tcp(user-shaggy.cd5kwh1iyrrb.us-east-1.rds.amazonaws.com:3306)/user_service_db?charset=utf8mb4&parseTime=True&loc=Local"
+
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
     if err != nil {
-        log.Fatal("Failed to connect to database:", err)
+        log.Fatalf("Failed to connect to database: %v", err)
     }
-    DB = database
+
+    DB = db
+    log.Println("MySQL database connected with GORM")
     return DB
 }
-
